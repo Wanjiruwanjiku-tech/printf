8d5e698a544ed40741a6dd8709f8163fb39a50ba
mosesgitonga <natalieshiro2016@gmail.com>
2023-03-27 04:25:34 -0700

    this is a function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,8 +1,8 @@
-#include <stdarg.h>  // for variable arguments list
-#include <stdio.h>   // for printf, sprintf, snprintf
-#include <stdlib.h>  // for malloc, free
-#include <string.h>  // for strlen, strncpy
-#include <unistd.h>  // for write
+#include <stdarg.h>  /* for variable arguments list*/
+#include <stdio.h>   /* for printf, sprintf, snprintf*/
+#include <stdlib.h>  /* for malloc, free*/
+#include <string.h>  /* for strlen, strncpy*/
+#include <unistd.h>  /* for write*/
 
 /**
  * _printf - Custom implementation of printf function
@@ -11,42 +11,41 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;         // Total number of characters printed
-    va_list args;          // Variable argument list
-    const char *p = format; // Pointer to traverse the format string
-
+    int count = 0;         /* Total number of characters printed*/
+    va_list args;          /* Variable argument list*/
+    const char *p = format; /* Pointer to traverse the format st*/
     va_start(args, format);
 
-    while (*p != '\0') // Traverse the format string until end-of-string
+    while (*p != '\0') /* Traverse the format string until end-of-string*/
     {
-        if (*p == '%') // Found a format specifier
+        if (*p == '%') /* Found a format specifier*/
         {
-            p++; // Move past the '%'
+            p++; /* Move past the '%'*/
 
-            // Check the format specifier and perform the corresponding action
+            /* Check the format specifier and perform the corresponding action*/
             switch (*p)
             {
-                case 'c': // Character
+                case 'c': /* Character*/
                     {
                         char c = va_arg(args, int);
                         count += write(1, &c, 1);
                         break;
                     }
 
-                case 's': // String
+                case 's': /* String*/
                     {
                         char *s = va_arg(args, char *);
                         count += write(1, s, strlen(s));
                         break;
                     }
 
-                case '%': // Percent sign
+                case '%': /* Percent sign*/
                     {
                         count += write(1, "%", 1);
                         break;
                     }
 
-                default: // Unknown format specifier
+                default: /* Unknown format specifier*/
                     {
                         count += write(1, "%", 1);
                         count += write(1, &(*p), 1);
@@ -54,15 +53,15 @@ int _printf(const char *format, ...)
                     }
             }
         }
-        else // Regular character
+        else /* Regular character*/
         {
             count += write(1, &(*p), 1);
         }
 
-        p++; // Move to the next character in the format string
+        p++; /*Move to the next character in the format string*/
     }
 
-    va_end(args); // End variable argument list
+    va_end(args); /* End variable argument list*/
 
-    return count; // Return total number of characters printed
+    return count; /* Return total number of characters printed*/
 }
